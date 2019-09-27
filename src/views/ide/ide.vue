6be64db8b2626b7ace8480f8ec81ed3a12edb5e0
Junyup Hong <junyup.hong@gmail.com>
2019-09-27 16:27:38 +0900

    ide style 변경

--- src/views/ide/ide.vue
+++ src/views/ide/ide.vue
@@ -8,8 +8,8 @@
         <v-tabs vertical color="#00b248">
           <v-tab style="border-bottom: solid 1px rgba(0, 178, 72, 0.4);"><v-icon>description</v-icon></v-tab>
           <v-tab style="border-bottom: solid 1px rgba(0, 178, 72, 0.4);"><v-icon>bug_report</v-icon></v-tab>
-          <v-btn class="tab-button" icon width="100%" style="border-radius: 0px;"
-            @click="ui.showTabItem = !ui.showTabItem"><v-icon class="icon" :showTabItem="ui.showTabItem">keyboard_arrow_right</v-icon>
+          <v-btn class="tab-button" icon width="100%" style="border-radius: 0px;" color="#00b248"
+            @click="ui.showTabItem = !ui.showTabItem"><v-icon class="icon" :showTabItem="ui.showTabItem">keyboard_arrow_left</v-icon>
           </v-btn>
           <v-tab-item class="tab-item">
             <div style="font-size: 24px; color: #00b248; font-weight: 600; text-align: center; margin: 12px 0;">문제</div>
