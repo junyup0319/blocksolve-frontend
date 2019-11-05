afeda71851003ec95f64c2254ac611cc2301bd00
Junyup Hong <junyup.hong@gmail.com>
2019-11-05 22:52:14 +0900

    main page

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -2,7 +2,12 @@
 // Auto-generated files main.vue
 
 <template>
-  <h1>main</h1>
+  <div class="main-wrapper">
+    <div class="image-area">
+      <v-img class="image" cover src="../../assets/main_image1.png"/>
+    </div>
+    <v-btn width="300" height="48" dark color="rgb(65, 165, 211)" style="font-size: 16px; font-weight: 600; margin: auto;">BLOCK SOLVE 시작하기</v-btn>
+  </div>
 </template>
 <script src='./main.ts'>
 
