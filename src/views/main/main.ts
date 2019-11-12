90ad7ded89418b54c8f291848db55fe5956239c6
Junyup Hong <junyup.hong@gmail.com>
2019-11-12 11:14:25 +0900

    main page button 연결

--- src/views/main/main.ts
+++ src/views/main/main.ts
@@ -2,9 +2,17 @@
 // Auto-generated files main.ts
 
 import { Vue, Component } from 'vue-property-decorator';
+import {auth} from '@/lib/firebase';
 
 @Component({})
 export default class Main extends Vue {
+  private clickStart() {
+    this.$store.state.user === null ? auth.signIn() : this.$router.push({
+      name: 'problem-list',
+      path: '/problems',
+    });
+
+  }
   private mounted() {
     //
   }
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -6,7 +6,9 @@
     <div class="image-area">
       <v-img class="image" cover src="../../assets/main_image1.png"/>
     </div>
-    <v-btn width="300" height="48" dark color="rgb(65, 165, 211)" style="font-size: 16px; font-weight: 600; margin: auto;">BLOCK SOLVE 시작하기</v-btn>
+    <!--TODO login이 되어있으면 문제 리스트로 이동 & 안돼있으면 login 으로 이동!-->
+    <v-btn width="300" height="48" dark color="rgb(65, 165, 211)" style="font-size: 16px; font-weight: 600; margin: auto;"
+      @click="clickStart">BLOCK SOLVE 시작하기</v-btn>
   </div>
 </template>
 <script src='./main.ts'>
